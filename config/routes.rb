11cb85ecc1f37aa222a3ed34c9da20a00faa67f5
Andy Xanthos <andykxanthos@gmail.com>
2016-10-30 14:29:59 +0000

    added more images and about page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'cars/list',     to: 'cars#list'
   get 'cars/show/:id', to: 'cars#show', as: 'show'
   get 'cars/all',      to: 'cars#all',  as: 'all'
+  get 'about',         to: 'static_pages#about', as: 'about'
 end
