951b5139a17d7a07ed9772fb49c98a5bebf71b90
Andy Xanthos <andykxanthos@gmail.com>
2016-11-07 14:50:20 +0000

    small styling changes, changes to subscribers_controller

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,15 +1,11 @@
 class SubscribersController < ApplicationController
   def create
-    if params[:email]  
-      @newsub = Subscriber.create({ email: params[:email]})
-      if @newsub.save
-        redirect_to '/'
-      else
-        redirect_to suberror_path
-      end
+    @newsub = Subscriber.create({ email: params[:email]})
+    if @newsub.save
+      redirect_to '/'
     else
       redirect_to suberror_path
-    end  
+    end
   end
 
   def edit
